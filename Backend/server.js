0b22ca4ad6540b420bf5d1b860dc843e82687d1d
rajeev <rajivg216@gmail.com>
2025-04-16 12:12:44 +0530

    Db Deployed On Railway

--- Backend/server.js
+++ Backend/server.js
@@ -62,8 +62,7 @@ const userRoutes = require("./routes/UserRoutes");
 app.use("/api/auth", userRoutes);
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, "localhost", () => {
+app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
    console.log("Swagger UI available at http://localhost:5000/api-docs");
 
